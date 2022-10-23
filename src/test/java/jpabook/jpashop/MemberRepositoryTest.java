465993ec4d3961363284dad094d03a6d26fd967d
lby132 <quddbscjswow@naver.com>
2022-10-23 18:43:43 +0900

    회원 도메인 개발 + 테스트 작성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,37 +1,59 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.service.MemberService;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
+@Transactional
 class MemberRepositoryTest {
 
     @Autowired
+    MemberService memberService;
+    @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
 
     @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() throws Exception {
+    void 회원가입() throws Exception {
         //given
         final Member member = new Member();
-        member.setUsername("memberA");
-
+        member.setName("lee");
         //when
-        final Long savedId = memberRepository.save(member);
-        final Member findMember = memberRepository.find(savedId);
+        final Long savedId = memberService.join(member);
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+        //given
+        final Member member1 = new Member();
+        member1.setName("lee");
+
+        final Member member2 = new Member();
+        member2.setName("lee");
+        //when
+        memberService.join(member1);
 
         //then
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        assertThat(findMember).isEqualTo(member);
-
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
     }
 
 }
