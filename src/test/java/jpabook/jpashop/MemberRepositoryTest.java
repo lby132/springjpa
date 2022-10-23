324e5e12bc32824a8c724c7e55c8bcba0ac8bb6d
lby132 <quddbscjswow@naver.com>
2022-10-23 23:50:27 +0900

    주문 도메인 개발

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,15 +1,12 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.service.MemberService;
+import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,107 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        final Member member = createMember();
+
+        final Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        //when
+        final Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        final Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
+
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        final Member member = createMember();
+        final Item item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 11;
+
+        //then
+        Assertions.assertThrows(NotEnoughStockException.class, () -> {
+            //when
+            orderService.order(member.getId(), item.getId(), orderCount);
+        });
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        final Member member = createMember();
+        final Book item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        final Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        //when
+        orderService.cancelOrder(orderId);
+        //then
+        final Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+    }
+
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        final Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        final Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+
+
+}
